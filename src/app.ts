acfbdffcf860e489a880119644d3aaa1687124f7
Albert Augusto <167814598+AlbertAugusto11@users.noreply.github.com>
2025-02-12 20:44:17 -0300

    Finalizado

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
 import express, { json } from "express";
+import { HandleErrors } from "./errors/handleErrors.middleware";
+import { booksRouter } from "./routes/books.routes";
 
 export const app = express();
 
-app.use(json());
+app.use(json())
+
+app.use("/books", booksRouter)
+
+app.use(HandleErrors.execute)
+
 
